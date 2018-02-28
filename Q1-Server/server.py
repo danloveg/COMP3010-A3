8519aec162621ae6c95e5ecaa2165ae4e57c6242
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-02-28 15:39:51 -0600

    Create server Python file

---
+++ Q1-Server/server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+"""
+ COMP3010 A1 - Q1
+
+ Author: Daniel Lovegrove
+
+ Version: Feb 28/2018
+
+ Purpose: Act as a web server and serve files to a client using only basic
+ sockets. No high level objects are allowed (such as ServerSocket).
+"""
+
+import socket
+
+HOST = ''
+ASSIGNED_PORT_NUM = 15048
+
