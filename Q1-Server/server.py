bc24adac94f8c05d0b58756fa707582eb4fe7dc3
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-02-28 18:53:30 -0600

    Accept client connections

--- Q1-Server/server.py
+++ Q1-Server/server.py
@@ -15,4 +15,42 @@ import socket
 
 HOST = ''
 ASSIGNED_PORT_NUM = 15048
+MAX_PACKET_SIZE = 2048
+address = (HOST, ASSIGNED_PORT_NUM)
+clientSocket = None
+serverSocket = None
+
+try:
+    # Create server socket, listen and accept connections
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.bind(address)
+    serverSocket.listen(socket.SOMAXCONN)
+    print "Server is listening for connections on port {0}".format(ASSIGNED_PORT_NUM)
+    clientSocket, clientAddress = serverSocket.accept()
+
+    # Receive data from Client
+    """
+    allDataRecvd = False
+    while allDataRecvd == False:
+        clientData = clientSocket.recv(2048).decode()
+        if not clientData:
+            allDataRecvd = True
+        else:
+            print "Waiting..."
+
+    print clientData
+    """
+
+except KeyboardInterrupt as k:
+    print "\nProgram interrupted. Exiting..."
+
+except Exception as e:
+    print "Error: {0}".format(e)
+
+finally:
+    if clientSocket:
+        clientSocket.close()
+    if serverSocket:
+        serverSocket.close()
 
