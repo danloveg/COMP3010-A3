dcb1191966f044ae4e2fcac541af104eadcf3d39
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-03-13 21:23:35 -0500

    Sucessfully have server connect to registry
    The server updates the date every second so that the client can have a
    fresh date every time they connect. The finest granularity in the date
    is seconds, so there is no reason for the server to update the object
    more often than that.

--- Q2-RMI/Daytime-RMI/DaytimeServer2.java
+++ Q2-RMI/Daytime-RMI/DaytimeServer2.java
@@ -8,25 +8,47 @@ import java.io.*;
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.util.IllegalFormatException;
+import java.net.MalformedURLException;
 import java.util.Date;
 
 public class DaytimeServer2 {
+    private static int NUM_SECS = 1;
+
     public static void main(String[] args) {
         try {
             if (args.length != 1) throw new IllegalArgumentException("Expects one argument");
             int port = Integer.parseInt(args[0]);
 
-            // Start the registry at the specified port
+            String currentDateUrl = "rmi://localhost:" + port + "/CurrentDate";
+
+            // Start the registry at the specified port, in this process
             LocateRegistry.createRegistry(port);
             System.out.println("Registry started.");
 
+            // Create an object in the registry
+            System.out.println("Server starting. Updating date every " + NUM_SECS + " seconds.");
+            while (true) {
+                // Get the current date
+                Date timestamp = new Date();
+                String date = timestamp.toString();
+                Naming.rebind(currentDateUrl, new CurrentDate(date));
+                // Sleep
+                Thread.sleep(NUM_SECS * 1000);
+            }
+
         } catch (NumberFormatException e) {
             System.out.println("Could not convert \"" + args[0] + "\" to an integer port number");
-        } catch (RemoteException e) {
-            System.out.println(e.getMessage());
         } catch (IllegalArgumentException e) {
             System.out.println("Expected one arguments. Call like:");
             System.out.println("\tjava DaytimeServer2 <port>");
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        } catch (ConnectException e) {
+            System.out.println(e.getMessage());
+        } catch (RemoteException e) {
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
