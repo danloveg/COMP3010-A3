f2a5688c6532d27f6b36f72688459564ada26cb1
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-03-13 20:29:57 -0500

    Start Object registry in server

--- Q2-RMI/Daytime-RMI/DaytimeServer2.java
+++ Q2-RMI/Daytime-RMI/DaytimeServer2.java
@@ -1,11 +1,12 @@
 /**
  * DaytimeServer2.java
- * 
+ *
  * Author: Daniel Lovegrove
  */
 
 import java.io.*;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
 import java.util.IllegalFormatException;
 import java.util.Date;
 
@@ -14,8 +15,15 @@ public class DaytimeServer2 {
         try {
             if (args.length != 1) throw new IllegalArgumentException("Expects one argument");
             int port = Integer.parseInt(args[0]);
+
+            // Start the registry at the specified port
+            LocateRegistry.createRegistry(port);
+            System.out.println("Registry started.");
+
         } catch (NumberFormatException e) {
             System.out.println("Could not convert \"" + args[0] + "\" to an integer port number");
+        } catch (RemoteException e) {
+            System.out.println(e.getMessage());
         } catch (IllegalArgumentException e) {
             System.out.println("Expected one arguments. Call like:");
             System.out.println("\tjava DaytimeServer2 <port>");
