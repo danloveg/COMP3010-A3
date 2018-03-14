cf9972bdd0e1598547c0b38e45c3756429f0dc03
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-03-14 08:39:09 -0500

    Update client url to rmi url

--- Q2-RMI/Daytime-RMI/DaytimeClient2.java
+++ Q2-RMI/Daytime-RMI/DaytimeClient2.java
@@ -7,7 +7,6 @@
 import java.io.*;
 import java.rmi.*;
 import java.util.IllegalFormatException;
-import java.net.MalformedURLException;
 
 public class DaytimeClient2 {
     public static void main(String[] args) {
@@ -15,9 +14,10 @@ public class DaytimeClient2 {
             if (args.length != 2) throw new IllegalArgumentException("Expects two arguments");
             String hostname = args[0];
             int port = Integer.parseInt(args[1]);
-            String currentDateUrl = "//" + hostname + ":" + port + "/CurrentDate";
+            String objectName = "CurrentDate";
+            String url = "rmi://" + hostname + ":" + port + "/" + objectName;
 
-            DateInterface date = (DateInterface) (Naming.lookup(currentDateUrl));
+            DateInterface date = (DateInterface) Naming.lookup(url);
             System.out.print("Here is the timestamp received from the server: ");
             System.out.println(date.echoDate());
 
@@ -26,12 +26,8 @@ public class DaytimeClient2 {
         } catch (IllegalArgumentException e) {
             System.out.println("Expected two arguments. Call like:");
             System.out.println("\tjava DaytimeClient2 <hostname> <port>");
-        } catch (MalformedURLException e) {
-            System.out.println(e.getMessage());
-        } catch (RemoteException e) {
-            System.out.println(e.getMessage());
-        } catch (NotBoundException e) {
-            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
