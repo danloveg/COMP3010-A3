d4cb360a4804658cdbc44e0f4229adb5f17dc84e
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-03-13 21:25:46 -0500

    Client connects to registry and outputs date

--- Q2-RMI/Daytime-RMI/DaytimeClient2.java
+++ Q2-RMI/Daytime-RMI/DaytimeClient2.java
@@ -7,6 +7,7 @@
 import java.io.*;
 import java.rmi.*;
 import java.util.IllegalFormatException;
+import java.net.MalformedURLException;
 
 public class DaytimeClient2 {
     public static void main(String[] args) {
@@ -14,11 +15,23 @@ public class DaytimeClient2 {
             if (args.length != 2) throw new IllegalArgumentException("Expects two arguments");
             String hostname = args[0];
             int port = Integer.parseInt(args[1]);
+            String currentDateUrl = "//" + hostname + ":" + port + "/CurrentDate";
+
+            DateInterface date = (DateInterface) (Naming.lookup(currentDateUrl));
+            System.out.print("Here is the timestamp received from the server: ");
+            System.out.println(date.echoDate());
+
         } catch (NumberFormatException e) {
             System.out.println("Could not convert \"" + args[1] + "\" to an integer port number");
         } catch (IllegalArgumentException e) {
             System.out.println("Expected two arguments. Call like:");
             System.out.println("\tjava DaytimeClient2 <hostname> <port>");
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        } catch (RemoteException e) {
+            System.out.println(e.getMessage());
+        } catch (NotBoundException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
